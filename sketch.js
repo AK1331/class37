cd048981c94f80f269251fe42c85bd2cfdc87953
AK1331 <73995475+AK1331@users.noreply.github.com>
2021-02-17 15:46:09 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+var database,canvas,backgroundImage,playerCount;
+var gameState = 0;
+var player,form,game;
+var allPlayers;
+
+function setup(){
+    database = firebase.database();
+    createCanvas(400,400);
+    game=new Game();
+    game.getState();
+    game.start();
+   
+}
+
+function draw(){
+    if (playerCount===4){
+        game.update(1);
+    }
+    if (gameState===1){
+        clear();
+        game.play();
+    }
+}
+
